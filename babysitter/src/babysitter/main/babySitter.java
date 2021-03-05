03c290ccc7af62bc089b8758b8577ff8dff94a01
Maulik Parikh <maulikparikh@Mauliks-MacBook-Pro.local>
2021-03-05 15:28:13 -0500

    pay_from_badtime_to_midnight
    added the pay_from_badtime_to_midnight

--- babysitter/src/babysitter/main/babySitter.java
+++ babysitter/src/babysitter/main/babySitter.java
@@ -34,9 +34,28 @@ public class babySitter {
         System.out.println("This is the pay for start time to bed time => $ "+pay);
         return pay;
     }
+
+    // below method is to calculate the pay from the bedtime  to midnight
+    private static int pay_from_badtime_to_midnight(int start, int bed, int end) {
+        int pay = 0;
+
+        if (bed <= MIDNIGHT) {
+        	pay += (MIDNIGHT - bed) * payfrombedtomidnight;
+        }
+        else if (start <= bed) {
+        	pay += (MIDNIGHT - bed) * payfrombedtomidnight;
+        }
+        else {
+            pay += (MIDNIGHT - start) * payfrombedtomidnight;
+        }
+        System.out.println("This is the pay for bed time to midnight => $ "+pay);
+        return pay;
+    }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		pay_from_Starttime_to_bedtime(5,8,4);
+        pay_from_badtime_to_midnight(5,8,4);
 
 	}
 
